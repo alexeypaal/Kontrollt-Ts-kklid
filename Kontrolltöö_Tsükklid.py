dbbcbb5f5b7bba8c3692f9201f4d9194c95b80c8
opilane <opilane@HP-CZC2349HTZ>
2023-01-18 15:39:50 +0200

    Add project files.

---
+++ Kontrolltöö_Tsükklid.py
@@ -0,0 +1,64 @@
+from math import *
+from random import *
+# 4
+amööb = 1
+for i in range(3, 25, 3,):
+    amööb *= 2
+    print(f"Pärast {i} tundi: {amööb} on kamber")
+    print()
+
+
+
+
+#3 (Füüsika hinded)
+print ("Keskmine hinne")
+import random
+n=random.randint(20,30)
+s=0
+for i in range(n):
+   a=randint(1,5)
+   s=s+a
+print(f"Keskmine hinne on {s/n}")
+print()
+
+
+
+
+
+#4 (3 ülesanne)
+kingitus=1
+aasta=1
+while kingitus<=100:
+    aasta+=1
+    kingitus+=aasta
+    print(aasta,"Aastat vana",kingitus,"Dollar")
+    print()
+
+
+
+
+#2 (Arv)
+p = int(input("Eksponent: "))
+n = int(input("Piir: "))
+i = 1
+while i ** p <= 100:
+    print(i ** p, end=' ')
+    i += 1
+print("\n Viimane arv,"
+      " Astendamine:", i - 1)
+print()
+
+
+
+#1 (Ekraaniloomad)
+from math import *
+from random import *
+print ("Ekraaniloomad")
+
+
+
+
+
+
+
+
